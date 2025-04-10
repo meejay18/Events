1e9e274aacaa84ef0fda449455a40b3b42fe54cc
digbanmije18@gmail.com <digbanmije18@gmail.com>
2025-04-10 22:39:01 +0100

    changes

---
+++ eventLoop.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+
+setTimeout(() => {
+  console.log("Timer 1 finished");
+}, 0);
+setImmediate(() => {
+  console.log("Immediate 1 finished");
+});
+
+fs.readFile("test-file.txt", () => {
+  console.log("Testing I/O");
+  setTimeout(() => {
+    console.log("Timer 2 finished");
+  }, 0);
+  setTimeout(() => {
+    console.log("Timer 3 finished");
+  }, 3000);
+  setImmediate(() => {
+    console.log("Immediate 2 finished");
+  });
+
+  console.log("----------");
+
+  process.nextTick(() => {
+    console.log("Process.nextTick");
+  });
+});
+
+console.log("Hello top level code");
